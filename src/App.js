e818809a6196c99014685dba2afec2fca6bb3e2e
Khoa Nguyen Luong <khoa.luong@yahoo.com.vn>
2019-11-25 22:01:37 -0800

    Finish layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, {Component} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import FILTERS from './filter.json';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 import logo from './img/logo.png';
 
 class App extends Component {
     render() {
         return (
-            <div id="mainBody">
-                <div className="body-flex">
-                    <div id="element-1">
-                        <Nav/>
-                    </div>
-                    <Main/>
+            <div className="body-flex">
+                <div id="element-1">
+                    <Nav/>
                 </div>
+                <Main/>
             </div>
         );
     }
@@ -69,7 +65,7 @@ class CoursePage extends Component {
         return (
         <div id="course-page">
             <SearchField/>
-            
+            <ResultField/>
         </div>
         );
     }
@@ -85,6 +81,7 @@ class SearchField extends Component {
                         placeholder="Enter department code (e.g INFO)"></input>
                     <button className="searchButton"><i className="fa fa-search" aria-label="search"></i></button>
                 </form>
+                <Filter filters={FILTERS}/>
             </section>
         );
     }
@@ -92,13 +89,41 @@ class SearchField extends Component {
 
 class Filter extends Component {
     render() {
-        let filterList = this.props.
+        let filterList = this.props.filters.map(element => {
+            return (
+                <FilterCard filter={element}/>
+            );
+        });
         return(
             <section className="subSection2">
+                {filterList}
+            </section>
+        );
+    }
+}
 
+class FilterCard extends Component {
+    render() {
+        let filter = this.props.filter;
+        let choices = this.props.filter.element.map(choice => {
+            console.log(choice);
+            console.log(filter.type + " " + filter.name + " " + choice.value + " " + choice.content);
+            return (
+               <p><input type={filter.type} name={filter.name} value={choice.value}></input>{choice.content}<br></br></p>
+            );
+        });
+        console.log(" ");
+    return <div className="customCard"><h3>{filter.header}</h3>{choices}</div>
+    }
+}
+
+class ResultField extends Component {
+    render() {
+        return (
+            <section className="subSection">
+                <h2>Recommended Courses</h2>
             </section>
         )
     }
 }
-
 export default App;
